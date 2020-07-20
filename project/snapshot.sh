46e6ee3630690924198fde62dace513c5be53d06
Cédric Chantepie <cchantep@users.noreply.github.com>
2020-07-20 22:08:23 +0200

    Skip snapshot publication for release

--- project/snapshot.sh
+++ project/snapshot.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 
+V=`sbt 'show version' 2>&1 | tail -n 1 | cut -d ']' -f 2 | perl -pe 's/^[ \t]+//;s/[ \t]+$//'`
+
+if [ `echo "$V" | grep -- '-SNAPSHOT' | wc -l` -eq 0 ]; then
+  echo "Cannot publish release $V"
+  exit 1
+fi
+
 export PUBLISH_REPO_NAME="Sonatype Nexus Repository Manager"
 export PUBLISH_REPO_ID="oss.sonatype.org"
 export PUBLISH_USER="cchantep"
