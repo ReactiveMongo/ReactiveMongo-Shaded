17a2a1b1e48532bf79e4c8df451f8e8d4fa44456
Scala Steward <me@scala-steward.org>
2021-07-16 14:59:07 +0200

    Update netty-transport-native-epoll, ... to 4.1.66.Final

--- project/Shaded.scala
+++ project/Shaded.scala
@@ -11,7 +11,7 @@ import sbtassembly.{
 object Shaded {
   import XmlUtil.transformPomDependencies
 
-  val nettyVer = "4.1.63.Final"
+  val nettyVer = "4.1.66.Final"
 
   lazy val commonModule = Project("ReactiveMongo-Shaded", file("shaded")).
     settings(
