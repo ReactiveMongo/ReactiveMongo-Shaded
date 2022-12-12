68f61356975931d27f57bed6f19b2ad452677170
Scala Steward <scala_steward@virtuslab.com>
2022-12-12 17:52:00 +0000

    Update netty-handler, ... to 4.1.86.Final

--- project/Shaded.scala
+++ project/Shaded.scala
@@ -11,7 +11,7 @@ import sbtassembly.{
 object Shaded {
   import XmlUtil.transformPomDependencies
 
-  val nettyVer = "4.1.85.Final"
+  val nettyVer = "4.1.86.Final"
 
   lazy val commonModule = Project("ReactiveMongo-Shaded", file("shaded")).
     settings(
