3a454ed6c349d0aa68ea4baf7b800b424bcc1fa4
Cédric Chantepie <cchantep@users.noreply.github.com>
2024-07-16 22:39:41 +0200

    Update build

--- project/snapshot.sh
+++ project/snapshot.sh
@@ -9,8 +9,9 @@ fi
 
 export PUBLISH_REPO_NAME="Sonatype Nexus Repository Manager"
 export PUBLISH_REPO_ID="oss.sonatype.org"
-export PUBLISH_USER="cchantep"
-export PUBLISH_REPO_URL="https://oss.sonatype.org/content/repositories/snapshots"
+#export PUBLISH_REPO_ID="s01.oss.sonatype.org"
+export PUBLISH_USER="daTPQpWq"
+export PUBLISH_REPO_URL="https://${PUBLISH_REPO_ID}/content/repositories/snapshots"
 
 echo "Password: "
 read PASS
