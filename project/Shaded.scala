ee54cd12cb98b0a8e9e62a69ddacbc88bc114334
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-08-14 21:52:36 +0200

    Update netty-codec-compression, ... to 4.2.4.Final (#181)

--- project/Shaded.scala
+++ project/Shaded.scala
@@ -11,7 +11,7 @@ import sbtassembly.{
 object Shaded {
   import XmlUtil.transformPomDependencies
 
-  val nettyVer = "4.2.3.Final"
+  val nettyVer = "4.2.4.Final"
 
   lazy val commonModule = Project("ReactiveMongo-Shaded", file("shaded")).
     settings(
